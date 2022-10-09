f897ebf0120c90b957926e9c637e7eb475010848
zlatan-dp <zlatandnepr@gmail.com>
2022-10-09 22:47:58 +0300

    fixed

--- src/components/stats-item/stats-item.jsx
+++ src/components/stats-item/stats-item.jsx
@@ -3,7 +3,11 @@ import { Wrap, StatLabel, StatPercentage } from './stat-item.styled';
 
 export const StatsItem = ({ data: { label, percentage } }) => {
   return (
-    <Wrap>
+    <Wrap
+      style={{
+        backgroundColor: getRandomHexColor(),
+      }}
+    >
       <StatLabel>{label}</StatLabel>
       <StatPercentage>{percentage}%</StatPercentage>
     </Wrap>
@@ -16,3 +20,9 @@ StatsItem.propTypes = {
     percentage: PropTypes.number,
   }),
 };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
